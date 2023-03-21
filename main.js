a8849d0a1bca477a16dd5ef75ef77e0ef9d04e5b
2009Parth <111286031+2009Parth@users.noreply.github.com>
2023-03-21 20:30:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+function preload()
+{
+
+}
+
+
+
+
+
+function setup()
+{
+    canvas = createCanvas(340,300);
+    canvas.center();
+}
+
+
+
+
+
+
+function draw()
+{
+
+}
+
+
+
+
+
+
+function take_snapshot()
+{
+    save('My_Filter_Image.png');
+}
